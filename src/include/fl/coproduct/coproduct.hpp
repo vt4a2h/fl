58ba22aa35d6d3115182cd9c669e7a20f354a1f4
Vitaly Fanaskov <vt4a2h@gmail.com>
2025-10-01 23:35:11 +0200

    Skeleton of Coproduct

---
+++ src/include/fl/coproduct/coproduct.hpp
@@ -0,0 +1,17 @@
+//
+// MIT License
+//
+// Copyright (c) 2024-present Vitaly Fanaskov
+//
+// fl -- Functional tools for C++
+// Project home: https://github.com/vt4a2h/fl
+//
+// See LICENSE file for the further details.
+//
+#pragma once
+
+#include <type_traits>
+
+namespace fl
+{
+} // namespace fl
---
+++ test/coproduct/test_coproduct.cpp
@@ -0,0 +1,17 @@
+//
+// MIT License
+//
+// Copyright (c) 2025-present Vitaly Fanaskov
+//
+// fl -- Functional tools for C++
+// Project home: https://github.com/vt4a2h/fl
+//
+// See LICENSE file for the further details.
+//
+#include "catch.hpp"
+
+#include <fl/coproduct/coproduct.hpp>
+
+TEST_CASE("Create coproduct")
+{
+}
