b1533318470b1fe0d5fb4aebb71141c7cae8cf60
Vitaly Fanaskov <vt4a2h@gmail.com>
2025-10-06 23:57:06 +0200

    A very basic implementation of coproduct match

--- src/include/fl/coproduct/coproduct.hpp
+++ src/include/fl/coproduct/coproduct.hpp
@@ -11,9 +11,26 @@
 #pragma once
 
 #include <type_traits>
+#include <limits>
 
 namespace fl
 {
+    namespace detail
+    {
+        struct coproduct_base_tag
+        {
+        };
+
+        template <class... Ts>
+        struct overloads : Ts...
+        {
+            using Ts::operator()...;
+        };
+    }
+
+    template <class... Args>
+    struct Coproduct;
+
     namespace detail
     {
         template <class...>
@@ -22,8 +39,11 @@ namespace fl
         };
     } // namespace detail
 
+    template <class MaybeCoproduct>
+    concept IsCoproduct = std::is_base_of_v<detail::coproduct_base_tag, std::remove_cvref_t<MaybeCoproduct>>;
+
     template <class... Args>
-    struct Coproduct
+    struct Coproduct : detail::coproduct_base_tag
     {
         static_assert(detail::always_false<Args...>::value, "Coproduct is not implemented for this number of types");
     };
@@ -61,7 +81,7 @@ namespace fl
     template <class... Args>
     constexpr std::size_t arity<Coproduct<Args...>> = sizeof...(Args);
 
-    template <class T, class P>
+    template <class, class P>
     constexpr bool holds_value_of_type(const P&)
     {
         return false;
@@ -81,5 +101,50 @@ namespace fl
         return p.is_v0
                    ? std::is_same_v<typename std::remove_cv_t<P>::value_t_0, T>
                    : std::is_same_v<typename std::remove_cv_t<P>::value_t_1, T>;
+    }
+
+    template <IsCoproduct P, class... Matchers>
+    decltype(auto) match(P&&, Matchers&&...)
+    {
+        static_assert(detail::always_false<P, Matchers...>::value,
+                      "Match is not implemented for this number of types");
+    }
+
+    template <IsCoproduct P, class Matcher>
+        requires (
+            arity<std::remove_cvref_t<P>> == 1 &&
+            std::is_invocable_v<Matcher, typename std::remove_cvref_t<P>::value_t>
+        )
+    decltype(auto) match(P&& p, Matcher&& m)
+    {
+        return std::invoke(std::forward<Matcher>(m), std::forward<P>(p.val));
+    }
+
+    template <IsCoproduct P, class... Matchers>
+        requires (
+            arity<std::remove_cvref_t<P>> == 2 &&
+            sizeof...(Matchers) == 2 &&
+            (... || (
+                std::is_invocable_v<Matchers, typename std::remove_cvref_t<P>::value_t_0> ||
+                std::is_invocable_v<Matchers, typename std::remove_cvref_t<P>::value_t_1>))
+        )
+    decltype(auto) match(P&& p, Matchers&&... m)
+    {
+        const auto matcher = detail::overloads{std::forward<Matchers>(m)...};
+
+        if (holds_value_of_type<typename std::remove_cvref_t<P>::value_t_0>(p))
+        {
+            if (std::is_invocable_v<decltype(matcher), typename std::remove_cvref_t<P>::value_t_0>)
+            {
+                return std::invoke(matcher, std::forward<P>(p.val).v0);
+            }
+        }
+        else
+        {
+            if (std::is_invocable_v<decltype(matcher), typename std::remove_cvref_t<P>::value_t_1>)
+            {
+                return std::invoke(matcher, std::forward<P>(p.val).v1);
+            }
+        }
     }
 } // namespace fl
